d6cc10a6dbb80247cb60a97bf259b8df3eee007e
Armintas <az778@york.ac.uk>
2022-06-10 20:48:36 +0200

    feat(base): Implemented the base of the player

--- src/components/Player/PlayerRoot/PlayerRoot.tsx
+++ src/components/Player/PlayerRoot/PlayerRoot.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
-export const PlayerRoot: React.FC = () => {
-	return <div>Player</div>;
+export const PlayerRoot: React.FC = ({ children }) => {
+	return (
+		<div
+			className="flex justify-center items-center bg-white fixed bottom-0 w-1/2 left-1/4 my-3 h-32"
+			style={{ boxShadow: '3px 3px 15px 2px rgba(0, 0, 0, 0.25)' }}
+		>
+			{children}
+		</div>
+	);
 };
